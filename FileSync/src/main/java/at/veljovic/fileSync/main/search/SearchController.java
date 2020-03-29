8872929a93f2bc1b3e28db5c774d7a19cf1f4c73
Aleks <a@veljovic.at>
2020-03-30 00:12:05 +0200

    .

--- FileSync/src/main/java/at/veljovic/fileSync/main/search/SearchController.java
+++ FileSync/src/main/java/at/veljovic/fileSync/main/search/SearchController.java
@@ -1,6 +1,8 @@
 package at.veljovic.fileSync.main.search;
 
+import java.awt.Desktop;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,11 +14,16 @@ import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.MouseEvent;
 
 public class SearchController implements FileSearchListener {
 
@@ -35,6 +42,8 @@ public class SearchController implements FileSearchListener {
 	@FXML
 	private ListView<File> resultListView;
 	private ObservableList<File> resultList = FXCollections.observableArrayList();
+	@FXML
+	private ContextMenu resultFileContextMenu;
 
 	private List<FileSearch> searchables = new ArrayList<>();
 
@@ -58,6 +67,45 @@ public class SearchController implements FileSearchListener {
 			search.addListener(this);
 			new Thread(search).start();
 		});
+	}
+
+	public void onClickresultListView(MouseEvent evt) {
+		if (evt.getClickCount() == 2) {
+			File selectedFile = resultListView.getSelectionModel().getSelectedItem();
+			try {
+				Desktop.getDesktop().open(selectedFile);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		} else if (evt.isSecondaryButtonDown()) {
+			resultFileContextMenu.show((Node) evt.getSource(), 0, 0);
+		}
+	}
+
+	public void onActionOpenFileMenuItem() {
+		File selectedFile = resultListView.getSelectionModel().getSelectedItem();
+		try {
+			Desktop.getDesktop().open(selectedFile);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void onActionShowInExplorerMenuItem() {
+		File selectedFile = resultListView.getSelectionModel().getSelectedItem();
+		try {
+			Runtime.getRuntime().exec("explorer.exe /select," + selectedFile.getAbsolutePath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void onActionCopyPathMenuItem() {
+		File selectedFile = resultListView.getSelectionModel().getSelectedItem();
+		ClipboardContent content = new ClipboardContent();
+		content.putString(selectedFile.getAbsolutePath());
+		Clipboard.getSystemClipboard().setContent(content);
 	}
 
 	public void onActionCancelButton() {
