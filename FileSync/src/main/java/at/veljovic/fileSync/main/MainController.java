3cd7fa04007264b192567dfb006e6a14a729706e
Aleks <a@veljovic.at>
2020-03-29 21:58:28 +0200

    .

--- FileSync/src/main/java/at/veljovic/fileSync/main/MainController.java
+++ FileSync/src/main/java/at/veljovic/fileSync/main/MainController.java
@@ -1,10 +1,17 @@
 package at.veljovic.fileSync.main;
 
-import at.veljovic.fileSync.main.search.SearchController;
 import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
 
 public class MainController {
-
 	@FXML
-	private SearchController searchController;
+	private MenuBar menuBar;
+	@FXML
+	private MenuItem closeMenuItem;
+
+	public void onActionCloseMenuItem() {
+		((Stage) menuBar.getScene().getWindow()).close();
+	}
 }
